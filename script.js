0cb797d5d8ff1b570ae35ee8e386e441a1fb5323
Limaaad <fabielledelima@gmail.com>
2025-06-03 19:33:07 -0300

    adicionando javascript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// 645fcb87f1d948c281a028fb48a3aaa2
+// Variáveis e seleção de elementos
+const apiKey = "645fcb87f1d948c281a028fb48a3aaa2";
+const apiCountryURL ="https://flagcdn.com/16x12";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+//Funções
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    console.log(city);
+});
