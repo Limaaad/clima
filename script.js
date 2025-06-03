850e8b4c2897a62b279831b796fee1364910450d
Limaaad <fabielledelima@gmail.com>
2025-06-03 20:43:34 -0300

    ja n sei mais nd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Variáveis e seleção de elementos
 const apiKey = "645fcb87f1d948c281a028fb48a3aaa2";
-const apiCountryURL ="https://flagcdn.com/16x12";
+const apiCountryURL ="https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -10,7 +10,7 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity");
+const humidityElement = document.querySelector("#humidity");
 const windElement = document.querySelector("#wind");
 
 //Funções
@@ -21,11 +21,22 @@ const getWeatherData = async(city) => {
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
-    console.log(data);
+    return(data);
 };
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src", 
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
 };
 
 // Eventos
