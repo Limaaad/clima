a0926a2da841f02c25bc88903087d03cd56051d4
Limaaad <fabielledelima@gmail.com>
2025-06-03 19:41:15 -0300

    adc elementos e funções

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const apiCountryURL ="https://flagcdn.com/16x12";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity");
+const windElement = document.querySelector("#wind");
+
 //Funções
+const showWeatherData = (city) => {
+    console.log(city);
+};
 
 // Eventos
 searchBtn.addEventListener("click", (e) => {
